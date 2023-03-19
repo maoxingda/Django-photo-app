78712d3c6c0474ccf52eb2e631c18f85462ac03c
maoxd <maoxingda@meican.com>
2023-03-19 08:14:13 +0800

    通过django-uuslug把中文tag转成拼音slug

--- config/photoapp/models.py
+++ config/photoapp/models.py
@@ -1,16 +1,36 @@
-'''Photoapp Models'''
+"""Photoapp Models"""
 
 from django.db import models
 
 from django.contrib.auth import get_user_model
 
 from taggit.managers import TaggableManager
+from taggit.models import Tag, TaggedItem
+
+import uuslug
+
+
+class CnTag(Tag):
+    class Meta:
+        proxy = True
+
+    def slugify(self, tag, i=None):
+        return uuslug.slugify(self.name)
+
+
+class CnTaggedItem(TaggedItem):
+    class Meta:
+        proxy = True
+
+    @classmethod
+    def tag_model(cls):
+        return CnTag
+
 
 class Photo(models.Model):
-    
     title = models.CharField(max_length=45)
-    
-    description = models.CharField(max_length=250) 
+
+    description = models.CharField(max_length=250)
 
     created = models.DateTimeField(auto_now_add=True)
 
@@ -18,7 +38,7 @@ class Photo(models.Model):
 
     submitter = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
 
-    tags = TaggableManager() 
+    tags = TaggableManager(through=CnTaggedItem)
 
     def __str__(self):
         return self.title
