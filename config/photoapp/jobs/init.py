095bd5f0d1fd4655e698ef7dabea19843ff465c8
maoxd <maoxingda@meican.com>
2023-03-21 00:04:49 +0800

    添加初始化job

---
+++ config/photoapp/jobs/init.py
@@ -0,0 +1,31 @@
+import os
+from glob import glob
+from pathlib import Path
+from pprint import pprint
+
+from django.contrib.auth.models import User
+from django.core.files import File
+from django_extensions.management.jobs import BaseJob
+
+from photoapp.models import CnTag, Photo
+
+
+class Job(BaseJob):
+    help = "初始化数据"
+
+    def execute(self):
+        for (root, dirs, files) in os.walk(os.getcwd() + '/pic', topdown=True):
+            user = User.objects.get(id=2)
+            for file in files:
+                path = Path(os.path.join(root, file))
+                with path.open(mode='rb') as f:
+                    tag = os.path.basename(root)
+                    photo = Photo(
+                        title=tag,
+                        description=tag,
+                        submitter=user,
+                    )
+                    photo.image = File(f, name=path.name)
+                    photo.save()
+                    # 必须先保存之后才能添加tag因为tag会用到对象ID
+                    photo.tags.add(tag)
